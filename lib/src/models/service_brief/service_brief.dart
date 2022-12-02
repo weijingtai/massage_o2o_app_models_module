782f3c22dd0c9ac822105511b0e4ca9262326e3d
wjt <wjt19951215@gmail.com>
2022-12-01 17:23:22 -0800

    export unavailable_service_base_model.dart

--- lib/src/models/service_brief/service_brief.dart
+++ lib/src/models/service_brief/service_brief.dart
@@ -1,4 +1,5 @@
 export './service_brief.dart';
 export './serving_service_base_model.dart';
 export './appointment_service_base_model.dart';
+export './unavailable_service_base_model.dart';
 export './sortable_service.dart';
