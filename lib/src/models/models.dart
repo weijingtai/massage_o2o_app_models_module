2041bd5961afd0bef2d2f559628c9d92f843434d
wjt <wjt19951215@gmail.com>
2023-01-17 16:54:14 -0800

    export online_host_user_model.dart

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -22,6 +22,7 @@ export 'base_user_info.dart';
 export 'host_user_model.dart';
 export 'master_user_model.dart';
 export 'online_master_user_model.dart';
+export 'online_host_user_model.dart';
 export 'order_assign_notification_model.dart';
 export 'order_model.dart';
 // export 'service_event_unit.dart';
@@ -38,6 +39,8 @@ export 'service/service_event_model.dart';
 
 export 'location/address.dart';
 export 'location/coordinate.dart';
+export 'location/location.dart';
+
 export 'location/host_location_model.dart';
 export 'location/master_location_model.dart';
 export 'location/distance_model.dart';
